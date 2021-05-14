deae77f09c5aaf9287e32e681f9eb44e85fba8ca
piggylongsnout <therealpiggylongsnout@gmail.com>
2021-05-14 13:37:40 -0700

    I hope this is right!

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/welcome", to: "welcome#index"
+  get "/demopage", to: "demopage#index"
 end
