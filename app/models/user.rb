f78ded9a2739b74ccaa1405e3959826955f7af59
Rene Capella <renecapella.dev@gmail.com>
2021-08-15 19:20:39 -0700

    NSCCS-10 removing downcase from user model in leu of using emails on log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
      has_secure_password
 
-     before_validation :downcase_username
-
      validates :username, presence: true, uniqueness: true, on: :create
      validates :email, presence: true, uniqueness: true, on: :create
      validates :password, presence: true, on: :create
@@ -11,10 +9,4 @@ class User < ApplicationRecord
      has_many :clients
      has_many :projects, :through => :project_users
 
-
-     private
-
-     def downcase_username
-       self.username = username.downcase if username.present?
-     end
 end
